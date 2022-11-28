bec260957707dc2122b4384c177df4b9e92ed3a7
Apricosma <113317992+Apricosma@users.noreply.github.com>
2022-11-28 12:56:54 -0600

    Add date/time to post

--- assets/script/utility-functions.js
+++ assets/script/utility-functions.js
@@ -21,11 +21,32 @@ function newPost() {
     element.appendChild(headUserName);
     headUserName.innerText = user.userName;
 
+    let date = new Date();
+    const month = date.toLocaleString('default', { month: 'long' });
+    let dateText = document.createElement('p');
+    dateText.classList.add('date');
+    dateText.innerText = `${month} ${date.getDate()}, ${date.getFullYear()}`;
+    element.appendChild(dateText);
+
+    let timeText = document.createElement('p');
+    timeText.classList.add('time');
+    timeText.innerText = `${currentTime(date)}`;
+    element.appendChild(timeText);
+
     let postText = document.createElement('p');
     postText.innerText = postInput.value;
     element.appendChild(postText);
+}
 
-    
+function currentTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    let timeString = hours + ':' + minutes + ' ' + ampm;
+    return timeString;
 }
 
 export { select, onEvent, newPost }
